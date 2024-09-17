6a04984c1f1f6210ac0d0fae48f411cbb71d7c4f
OHRRRRR <3819713@naver.com>
2024-09-18 02:29:22 +0900

    fix - git stash 오류 해결

--- ShareTime/src/main/java/com/ureca/study/controller/StudyController.java
+++ ShareTime/src/main/java/com/ureca/study/controller/StudyController.java
@@ -2,6 +2,8 @@ package com.ureca.study.controller;
 
 import com.ureca.study.dto.Study;
 import com.ureca.study.model.service.StudyService;
+import com.ureca.user.util.JwtUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,9 @@ public class StudyController {
 
     @Autowired
     private StudyService studyService;
+    
+    @Autowired
+    private JwtUtil jwtUtil; // JwtUtil 주입
 
     @PostMapping("/checkExistence")
     public Map<String, String> checkExistence(@RequestBody Map<String, String> request) {
@@ -47,29 +52,53 @@ public class StudyController {
         return response;
     }
 
-    // 새로 추가된 엔드포인트
+ // StudyController.java
+
     @PostMapping("/joinOrCreate")
-    public Map<String, String> joinOrCreateStudy(@RequestBody Map<String, String> request) {
-        String study_name = request.get("study_name");
-        String study_key = request.get("study_key");
-        int user_id = Integer.parseInt(request.get("user_id")); // 사용자 ID를 받도록 추가
+    public Map<String, String> joinOrCreateStudy(
+            @RequestBody Map<String, String> request,
+            @RequestHeader("Authorization") String token) {
 
         Map<String, String> response = new HashMap<>();
-        if (studyService.studyExists(study_name)) {
-            boolean isValid = studyService.validateStudyKey(study_name, study_key);
-            if (isValid) {
-                studyService.addUserToStudy(user_id, study_name); // 스터디에 사용자 추가
-                response.put("message", "JOIN_SUCCESS");
-            } else {
-                response.put("message", "INVALID_KEY");
+
+        // JWT 토큰에서 user_id 추출
+        String extractedToken = token.replace("Bearer ", "");
+        Integer user_id;
+        try {
+            user_id = jwtUtil.extractUserId(extractedToken);
+            if (user_id == null) {
+                response.put("message", "INVALID_TOKEN");
+                return response;
             }
-        } else {
-            Study study = new Study();
-            study.setStudy_name(study_name);
-            study.setStudy_key(study_key);
-            studyService.createStudy(study);
-            studyService.addUserToStudy(user_id, study_name); // 새로운 스터디에 사용자 추가
-            response.put("message", "CREATED");
+        } catch (Exception e) {
+            response.put("message", "TOKEN_ERROR");
+            response.put("error", e.getMessage());
+            return response;
+        }
+
+        String study_name = request.get("study_name");
+        String study_key = request.get("study_key");
+
+        try {
+            if (studyService.studyExists(study_name)) {
+                boolean isValid = studyService.validateStudyKey(study_name, study_key);
+                if (isValid) {
+                    studyService.addUserToStudy(user_id, study_name); // 스터디에 사용자 추가
+                    response.put("message", "JOIN_SUCCESS");
+                } else {
+                    response.put("message", "INVALID_KEY");
+                }
+            } else {
+                Study study = new Study();
+                study.setStudy_name(study_name);
+                study.setStudy_key(study_key);
+                studyService.createStudy(study);
+                studyService.addUserToStudy(user_id, study_name); // 새로운 스터디에 사용자 추가
+                response.put("message", "CREATED");
+            }
+        } catch (Exception e) {
+            response.put("message", "ERROR");
+            response.put("error", e.getMessage());
         }
 
         return response;
