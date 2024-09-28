a6ae44fcbddf0b3abe94d14e404992f8954476e8
OHRRRRR <3819713@naver.com>
2024-09-28 18:33:47 +0900

    feat - user정보 조회

--- ShareTime/src/main/java/com/ureca/user/model/service/UserService.java
+++ ShareTime/src/main/java/com/ureca/user/model/service/UserService.java
@@ -6,17 +6,16 @@ import java.util.List;
 import com.ureca.user.dto.User;
 
 public interface UserService {
-//명세 => User 추가/수정/삭제/조회/모두조회
+    // 명세 => User 추가/수정/삭제/조회/모두조회
+    public int insert(User user) throws SQLException;
 
-	public int insert(User user) throws SQLException;
+    public int update(User user) throws SQLException;
 
-	public int update(User user) throws SQLException;
+    public int delete(int id) throws SQLException; // String에서 int로 수정
 
-	public int delete(String id) throws SQLException;
+    public User login(User user) throws SQLException;
 
-	public User login(User user) throws SQLException;
+    public User select(int id) throws SQLException; // String에서 int로 수정
 
-	public User select(String id) throws SQLException;
-
-	public List<User> selectAll() throws SQLException;
+    public List<User> selectAll() throws SQLException;
 }
